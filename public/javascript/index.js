1755d544ac50d8f943eab1aaea90cd29316687fb
Pamela Hermanto <phermanto@wgen.net>
2012-01-19 15:38:46 -0500

    Implement requestee submit button

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,5 +1,14 @@
-
+var tripRequestClickHandler = function (event) {
+    var requestee = $("#requestee").val();
+    var pick_up = $("#pick_up_address").val();
+    var drop_off = $("#drop_off_address").val();
+    alert("Request Mapped! Requestee: " + requestee + ", Pick-up: " + pick_up + ", Drop-off: " + drop_off);
+    // clear values
+    $("#requestee").val("");
+    $("#pick_up_address").val("");
+    $("#drop_off_address").val("");
+};
 
 $(document).ready(function () {
-    $('.map').append("ahhhh");
+    $('#trip_request').click(tripRequestClickHandler);
 });
