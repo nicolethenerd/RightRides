a4e812ec47f5d2e65f753706d3659df34d1e863c
Pamela Hermanto <phermanto@wgen.net>
2012-01-19 14:36:24 -0500

    To import local javascript

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,19 @@
-
+var $ = require('jquery');
 /*
  * GET home page.
  */
 
+function locationCallback(json) {
+        console.log(json);
+        alert("Longitude: " + json.location.position.longitude +"\nLatitude: " + json.location.position.latitude);
+      }
+
+
 exports.index = function(req, res){
-  res.render('index', { title: 'Right Rides Portal!' })
+  var url = "https://api.geoloqi.com/1/location/last?oauth_token=";
+  var url_with_token = url + "?callback=?&oauth_token=5276-09d6f72728ee71713875d5d2c93304ba90ec040b";
+  $.getJSON(url_with_token, locationCallback);
+  console.log("SOMETHING!");
+
+  res.render('index', { title: 'Right Rides Portal!'});
 };
