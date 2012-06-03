e07ce853c463672e92b9e6bf27732b803ac887ba
Milen Pavlov <mpavlov@wgen.net>
2012-06-03 17:32:35 -0400

    fixed Johny's boo-boo (I think)

--- public/javascript/mapsupport.js
+++ public/javascript/mapsupport.js
@@ -1,80 +1,88 @@
 var map;
 var geocoder;
 var customMarkers = [];
+var directionsDisplay;
+var directionsService;
+var serviceAreas = new google.maps.KmlLayer('http://maps.google.com/maps/ms?ie=UTF8&oe=UTF8&authuser=0&msa=0&output=nl&msid=209738999438525933783.00000111e2265debed28b');
 var geoloqi_refresh_rate = 15000; //15 sec
+
 function initializeMapping() {
-    var myOptions = {
-        center: new google.maps.LatLng(40.767781718519, -73.985238918519),
-        zoom: 11,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    geocoder = new google.maps.Geocoder();
-
-    map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-    var serviceAreas = new google.maps.KmlLayer('http://maps.google.com/maps/ms?ie=UTF8&oe=UTF8&authuser=0&msa=0&output=nl&msid=209738999438525933783.00000111e2265debed28b');
-    serviceAreas.setMap(map);
-
-
-   setTimeout("autoRefreshGeoloqi();", geoloqi_refresh_rate);
-  }
+  var myOptions = {
+    center: new google.maps.LatLng(40.767781718519, -73.985238918519),  // New York City
+    zoom: 11,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
   
-  function autoRefreshGeoloqi(){
-    refreshMarkers();
-    setTimeout("autoRefreshGeoloqi();", geoloqi_refresh_rate);
-  }
+  map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+  geocoder = new google.maps.Geocoder();
+  directionsDisplay = new google.maps.DirectionsRenderer();
+  directionsService = new google.maps.DirectionsService();
+ 
+  serviceAreas.setMap(map);
+  directionsDisplay.setMap(map);
 
-  function refreshMarkers(){        
-    var pending_geoloqi_calls = gl_pendingRequests();
-
-    if(pending_geoloqi_calls == 0){
-      console.log("Num of customMarkers: " +customMarkers.length);
-      clearMarkers();
-      gl_refreshAll();
-    }else{
-      console.log("Skipping call to geoloqi, " + pending_geoloqi_calls + " calls currently open");
-    }
-  }
-
-  function drawMarker(profile, latitude, longitude) {
-    console.log("drawing " + profile.display_name + " at " + latitude + "," + longitude);
-    var marker;
-    var infowindow = new google.maps.InfoWindow();
-
-    marker = new google.maps.Marker({
-        position: new google.maps.LatLng(latitude, longitude),
-        map: map,
-        icon: '/images/car_' + profile.display_name.toLowerCase() + '.png'
-    });
-
-    console.log(marker)
-
-    google.maps.event.addListener(marker, 'click', (function(marker, profile) {
-        return function() {
-            var html = "<table border='0'><tr>" +
-            "<td><img src='" + profile.profile_image + "' /></td>" +
-            "<td>" + profile.display_name + "<br />" + profile.phone + "</td>" +
-            "</tr></table>";
-            infowindow.setContent(html);
-            infowindow.open(map, marker);
-        }
-    })(marker, profile));
+  setTimeout("autoRefreshGeoloqi();", geoloqi_refresh_rate);
+}
+  
+function autoRefreshGeoloqi(){
+  refreshMarkers();
+  setTimeout("autoRefreshGeoloqi();", geoloqi_refresh_rate);
 }
 
-function geocode(this_address) {
-    //var address = document.getElementById("address").value;			
-    geocoder.geocode({
-        'address': this_address,
-        'partialmatch': true
-    },
-    geocodeResult);
+function refreshMarkers(){        
+  var pending_geoloqi_calls = gl_pendingRequests();
+
+  if(pending_geoloqi_calls == 0){
+    console.log("Num of customMarkers: " +customMarkers.length);
+    clearMarkers();
+    gl_refreshAll();
+  }else{
+    console.log("Skipping call to geoloqi, " + pending_geoloqi_calls + " calls currently open");
+  }
+}
+
+function drawMarker(profile, latitude, longitude) {
+  console.log("drawing " + profile.display_name + " at " + latitude + "," + longitude);
+  var marker;
+  var infowindow = new google.maps.InfoWindow();
+
+  marker = new google.maps.Marker({
+      position: new google.maps.LatLng(latitude, longitude),
+      map: map,
+      icon: '/images/car_' + profile.display_name.toLowerCase() + '.png'
+  });
+
+  console.log(marker)
+
+  google.maps.event.addListener(marker, 'click', (function(marker, profile) {
+      return function() {
+        var html = "<table border='0'><tr>" +
+          "<td><img src='" + profile.profile_image + "' /></td>" +
+          "<td>" + profile.display_name + "<br />" + profile.phone + "</td>" +
+          "</tr></table>";
+          infowindow.setContent(html);
+          infowindow.open(map, marker);
+      }
+  })(marker, profile));
 }
 
 function clearMarkers() {
-    while (customMarkers.length > 0)
-    {
-        marker = customMarkers.pop();
-        marker.setMap(null);
-    }
+   while(customMarkers.length > 0)
+   {
+    marker = customMarkers.pop();
+    marker.setMap(null);
+   }
+}
+
+function geocode(address_string) {
+  addresses = address_string.split(" to ");
+  if(addresses.length == 1) {
+    geocoder.geocode({'address': address_string, 'partialmatch': true}, geocodeResult);   
+  } else if(addresses.length == 2) {
+    displayDirections(addresses);
+  } else {
+    alert("Multiple destinations are not supported yet.");
+  }
 }
 
 function geocodeResult(results, status) {
@@ -90,4 +98,20 @@ function geocodeResult(results, status) {
     } else {
         alert("Geocode was not successful for the following reason: " + status);
     }
+}
+
+function displayDirections(addresses) {
+  request = {
+    origin: addresses[0],
+    destination: addresses[1],
+    travelMode: google.maps.TravelMode.DRIVING,
+    region: 'United States'
+  }
+  directionsService.route(request, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(response);
+    } else {
+      console.log("DirectionsService was not successful for the following reason: " + status)
+    }
+  });
 }
